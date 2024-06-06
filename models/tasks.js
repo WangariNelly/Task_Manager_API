f51cf1e4d769b58c822ea75825057021e5c2bd91
WangariNelly <nellywangari30@gmail.com>
2024-06-07 00:10:06 +0300

    Setting up the schema and validation

---
+++ models/tasks.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const TaskSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+        maxlength: 20
+    }, 
+    completed: {
+       type: Boolean,
+       default: false
+    }
+});
+
+
+
+export default mongoose.model('Task', TaskSchema);
