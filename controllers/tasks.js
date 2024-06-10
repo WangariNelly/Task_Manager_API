ed3e5460f40a4beaba2cc248342ee5c47d714db4
WangariNelly <nellywangari30@gmail.com>
2024-06-10 09:29:55 +0300

    Async wrapper module

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,56 +1,40 @@
 import Task from '../models/tasks.js'
 import asyncWrapper from '../middleware/async.js';
 
-export const getAllTasks = async (req,res) => {
-    try{
+export const getAllTasks = asyncWrapper(async (req,res) => { 
     const tasks = await Task.find({});
-     res.status(200).json({ tasks })
-    }catch (err){
-        res.status(500).json({ msg: err});
-    }
-};
+     res.status(200).json({ tasks });
+});
 
 
-export const createTasks = async (req,res) => {
-    try{
+export const createTasks = asyncWrapper(async (req,res) => {
     const task = await Task.create(req.body)
     res.status(201).json({ task })
-} catch (err){
-    res.status(500).json({ msg: err })
-}
-};
+});
 
 
-export const getTask = async (req,res) => {
-    try{
+export const getTask = asyncWrapper(async (req,res) => {
         const {id:taskID} = req.params
         const task = await Task.findOne({_id:taskID})
         if(!task){
            return res.status(404).json({ msg: `No task with this ID` }) 
         }
         res.status(200).json({ task }) 
-    }catch (err){
-    res.status(500).json({ msg: err })
-    }
-};
+});
 
 
-export const deleteTasks = async (req,res) => {
-    try{
+export const deleteTasks = asyncWrapper(async (req,res) => {
+  
         const {id:taskID} = req.params;
         const task = await Task.findOneAndDelete({_id:taskID});
         if(!task){
             return res.status(404).json({ msg: `No task with this ID` }) 
          }
          res.status(200).json({ task }) 
-    }catch (err){
-        res.status(500).json({ msg: err })
-        }
-    };
+    });
   
 
-    export const updateTasks = async (req,res) => {
-        try{
+    export const updateTasks = asyncWrapper(async (req,res) => {
             const {id:taskID} = req.params;
            const task = await Task.findByIdAndUpdate({_id:taskID},req.body, {
             new:true, runValidators:true
@@ -59,7 +43,4 @@ export const deleteTasks = async (req,res) => {
             return res.status(404).json({ msg: `No task with this ID` }) 
          }
          res.status(200).json({ task }) 
-    }catch (err){
-        res.status(500).json({ msg: err })
-        }
-    }
+    });
--- middleware/async.js
+++ middleware/async.js
@@ -1,5 +1,11 @@
-const asyncWrapper = () => {
-
+const asyncWrapper = (callback) => {
+return async (req,res,next) => {
+    try{
+        await callback(req,res,next)
+    } catch(err) {
+        next(err)
+    }
+}
 };
 
 
